4326f1b3e884156960728a64a342559163fc6b77
Raí Siqueira <rai93siqueira@gmail.com>
2017-09-15 16:28:20 -0300

    adjust the file foods.search.js

--- examples/foods/foods.search.js
+++ examples/foods/foods.search.js
@@ -4,10 +4,11 @@ const fatAPI = require('../fatAPI');
 
 // search for a food
 fatAPI
-  .method('foods.search', {
-    search_expression: 'Coffiest',
-    max_results: 10
-  })
-  .then(function(results) {
-    console.json(results.foods.food);
-  });
+.method('foods.search', {
+  search_expression: 'Coffiest',
+  max_results: 10
+})
+.then(function(results) {
+  console.log(results.foods.food);
+})
+.catch(err => console.error(err));
